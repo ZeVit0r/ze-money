c7033416b7720bf1269dbff3b9e7a2099b3158b8
Zé Vitor <z3.vit07@gmail.com>
2022-03-08 23:43:51 -0300

    add styles

--- src/components/TransactionsTable/styles.ts
+++ src/components/TransactionsTable/styles.ts
@@ -3,6 +3,15 @@ import styled from "styled-components";
 export const Container = styled.div`
     margin-top: 4rem;
 
+    @media(max-width: 768px) {
+        overflow-x: scroll;
+        overflow-y: hidden;
+        white-space: nowrap;
+        &::-webkit-scrollbar {
+            display: none;
+        } 
+    }
+
     table {
         width: 100%;
         border-spacing: 0 0.5rem;
